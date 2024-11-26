edcd3aa4ca87cc090243d5fc72da2c64818ab64f
Azzpect <atanughosh.brp@gmail.com>
2024-11-26 15:16:29 +0530

    added log out feature

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,12 +1,21 @@
+import { useContext } from "react"
+import { NotificationContext, UserContext } from "./contexts/AppContexts"
 
 
 export default function Menu() {
+
+    const {changeValidUser} = useContext(UserContext)
+    const {changeNotification} = useContext(NotificationContext)
+
     return(
         <div id="menu" className="menu">
             <h3>Profile</h3>
             <h3>Friends</h3>
             <h3 onClick={() => {
                 document.querySelector("#menu")?.classList.remove("active-menu")
+                localStorage.removeItem("userId")
+                changeValidUser(false)
+                changeNotification("success", "Logged out from the account")
             }}>LogOut</h3>
         </div>
     )
