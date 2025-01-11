fee34214cdec965c4e88de34d2d355ecbf5de556
Azzpect <atanughosh.brp@gmail.com>
2025-01-11 12:51:08 +0530

    new production script

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-
 export async function connectToDB() {
-    try {
-        await mongoose.connect(process.env.DB_URI as string)
-        console.log("Connected to database");
-    } catch (err) {
-        console.log("Error connecting to database");
-    }
+  try {
+    console.log(process.env.DB_URI);
+    await mongoose.connect(process.env.DB_URI as string);
+    console.log("Connected to database");
+  } catch (err) {
+    console.log("Error connecting to database");
+  }
 }
